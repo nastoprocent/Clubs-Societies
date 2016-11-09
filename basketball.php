d1756c63de7611841f159e8b0533c9e7c27f2979
sumit <tsumit07@gmail.com>
2016-11-09 19:48:03 +0000

    BasketBall

--- basketball.php
+++ basketball.php
@@ -127,7 +127,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Gaming</title>
+    <title>Basket Ball</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -219,7 +219,7 @@
                         <small>Welcome to</small>
                     </h2>
 
-                    <h1 class="brand-name"> Basketball Club</h1></h1>
+                    <h1 class="brand-name">BasketBall Society</h1>
                     <hr class="tagline-divider">
 
                 </div>
@@ -231,147 +231,14 @@
             <div class="row">
                 <div class="box">
                     <div class="col-md-8">
-                        <img class="img-responsive img-rounded" src="img/wearegamers.jpg" alt="">
+                        <img class="img-responsive img-rounded" src="img/pool4.jpg" alt="">
                     </div>
                     <div class="col-md-4">
-                        <h1>Gaming Society</h1>
-                        <p>Welcome to the NCI Gaming Soc page! Our aim to provide students of NCI a competive and fun gaming experience in the college. We will play a variety of games from console gaming platforms such as playstation, Xbox and PC and even retro consoles! 
+                        <h1>BasketBall Society</h1>
+                        <p>Welcome to the NCI BasketBall Society page! Our aim to provide students of NCI a fun BasketBall experience in the college as well as competing against other colleges in tournaments.  
 
-
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12">
-                    <hr>
-                    <h2 class="intro-text text-center">
-                        <strong>Sign Up</strong>
-                    </h2>
-                    <hr>
-                    <p><h6><center> Please fill in the form for signing up Gaming C&S</center></h6></p>
-                    
-                        <form class="form-horizontal" action = "savetoxml.php" method = "post">
-                            <div class="form-group">
-                                  <label class="col-sm-2 control-label">Name</label>
-                                  <div class="col-sm-4">
-                                    <input class="form-control" id="focusedInput" type="text" required placeholder="E.g John Murphy">
-                                  </div>
-                            </div>
-                            <div class="form-group">
-                                  <label class="col-sm-2 control-label">Student Email</label>
-                                  <div class="col-sm-6">
-                                    <input class="form-control" id="focusedInput" type="text" required  placeholder="x14656423@student.ncirl.ie">
-                                  </div>
-                            </div>
-                            <div class="form-group">
-                                  <label class="col-sm-2 control-label">Phone Number</label>
-                                  <div class="col-sm-6">
-                                    <input class="form-control" id="focusedInput" type="text" required  placeholder="08x xxx xxx">
-                                  </div>
-                            </div>
-                            <div class="form-group" >
-                                  <label class="col-sm-2 control-label">DOB</label>
-                                  <div class="col-sm-6">
-                                    <select name='day' id='dayddl'>
-                                            <option value='1'>1</option>
-                                            <option value='2'>2</option>
-                                            <option value='3'>3</option>
-                                            <option value='4'>4</option>
-                                            <option value='5'>5</option>
-                                            <option value='6'>6</option>
-                                            <option value='7'>7</option>
-                                            <option value='8'>8</option>
-                                            <option value='9'>9</option>
-                                            <option value='10'>10</option>
-                                            <option value='11'>11</option>
-                                            <option value='12'>12</option>
-                                            <option value='13'>13</option>
-                                            <option value='14'>14</option>
-                                            <option value='15'>15</option>
-                                            <option value='16'>16</option>
-                                            <option value='17'>17</option>
-                                            <option value='18'>18</option>
-                                            <option value='19'>19</option>
-                                            <option value='20'>20</option>
-                                            <option value='21'>21</option>
-                                            <option value='22'>22</option>
-                                            <option value='23'>23</option>
-                                            <option value='24'>24</option>
-                                            <option value='25'>25</option>
-                                            <option value='26'>26</option>
-                                            <option value='27'>27</option>
-                                            <option value='28'>28</option>
-                                            <option value='29'>29</option>
-                                            <option value='30'>30</option>
-                                            <option value='31'>31</option>
-                                        </select>
-                                        <select name='month' id='monthddl'>
-                                            <option value='1'>January</option>
-                                            <option value='2'>Februray</option>
-                                            <option value='3'>March</option>
-                                            <option value='4'>April</option>
-                                            <option value='5'>May</option>
-                                            <option value='6'>June</option>
-                                            <option value='7'>July</option>
-                                            <option value='8'>August</option>
-                                            <option value='9'>September</option>
-                                            <option value='10'>October</option>
-                                            <option value='11'>NOvember</option>
-                                            <option value='12'>December</option>
-                                            
-                                        </select>
-                                            
-                                        <select name='day' id='blah'>
-                                            <option value='1970'>1970</option>
-                                            <option value='1971'>1971</option>
-                                            <option value='1972'>1972</option>
-                                            <option value='1973'>1973</option>
-                                            <option value='1974'>1974</option>
-                                            <option value='1975'>1975</option>
-                                            <option value='1976'>1976</option>
-                                            <option value='1977'>1977</option>
-                                            <option value='1978'>1978</option>
-                                            <option value='1979'>1979</option>
-                                            <option value='1980'>1980</option>
-                                            <option value='1981'>1981</option>
-                                            <option value='1982'>1982</option>
-                                            <option value='1983'>1983</option>
-                                            <option value='1984'>1984</option>
-                                            <option value='1985'>1985</option>
-                                            <option value='1986'>1986</option>
-                                            <option value='1987'>1987</option>
-                                            <option value='1988'>1988</option>
-                                            <option value='1989'>1989</option>
-                                            <option value='1990'>1990</option>
-                                            <option value='1991'>1991</option>
-                                            <option value='1992'>1992</option>
-                                            <option value='1993'>1993</option>
-                                            <option value='1994'>1994</option>
-                                            <option value='1995'>1995</option>
-                                            <option value='1996'>1996</option>
-                                            <option value='1997'>1997</option>
-                                            <option value='1998'>1998</option>
-                                            <option value='1999'>1999</option>
-                                            <option value='2000'>2000</option>
-                                            <option value='2001'>2001</option>
-                                            <option value='2002'>2002</option>
-                                            <option value='2003'>2003</option>
-                                            <option value='2004'>2004</option>
-                                            <option value='2005'>2005</option>
-                                            <option value='2006'>2006</option>
-                                            <option value='2007'>2007</option>
-                                            <option value='2008'>2008</option>
-                                            <option value='2009'>2009</option>
-                                            <option value='2010'>2010</option>
-                                            <option value='2011'>2011</option>
-                                            <option value='2012'>2012</option>
-                                            <option value='2013'>2013</option>
-                                            <option value='2014'>2014</option>
-                                            <option value='2015'>2015</option>
-                                            <option value='2016'>2016</option>
-                                        </select>
-                                  </div>
-                            </div>
-                            
-                            If you post any form of spam on this page I will unleash my angry ban hammer and it upsets our team greatly. </p>
+                            Any suggestions or ideas please feel free to post it on our group page. Your feedback is important as it's your society!
+                             </p>
                     </div>
                 </div>
             </div>
@@ -381,22 +248,22 @@
             <div class="row">
                 <div class="box">
                     <div class="col-md-4">
-                        <names>Josephine Andrews</names><br /><br />
-                        <img border="0" src="clubsandsocsreps/gaming/gamingrep3.jpg" height="350"></img>
+                        <names></names><br /><br />
+                        <img border="0" src="clubsandsocsreps/BasketBall/#.jpg" height="350"></img>
                         <br /><br />
-                        <p><strong>Co-President:</strong> Josephine Andrews</p>
+                        <p><strong>President:</strong> ABC</p>
                     </div>
                     <div class="col-md-4">
-                        <names>Cillian John Murray</names><br /><br />
-                        <img border="0" src="clubsandsocsreps/gaming/gamingrep2.jpg" height="350"></img>
+                        <names></names><br /><br />
+                        <img border="0" src="clubsandsocsreps/BasketBall/#.jpg" height="350"></img>
                         <br /><br />
-                        <p><strong>Vice President:</strong> Cillian John Murray</p>
+                        <p><strong>Vice President:</strong> JKL</p>
                     </div>
                     <div class="col-md-4">
-                        <names>Ben Kibabu</names><br /><br />
-                        <img border="0" src="clubsandsocsreps/gaming/gamingrep1.jpg" height="350"></img>
+                        <names></names><br /><br />
+                        <img border="0" src="clubsandsocsreps/BasketBall/#.jpg" height="350"></img>
                         <br /><br />
-                        <p><strong>Creator of FB-Fanpage: </strong>Ben Kibabu</p>
+                        <p><strong>Creator of FB-Fanpage: </strong>XYZ</p>
                     </div>
                 </div>
             </div>
@@ -404,10 +271,10 @@
         <!-- Content Row -->
         
     	<div class="row">
-            <div class="box">
+            <div class="box" align="center">
                 <form name="myform" method="post" action="clubsoc.php" onSubmit="alert('Thank you for your application!!!!');">
                     <div class="form-group">
-                        <label for="Name">Name</label>
+                        <label for="Name" align="left">Name</label>
                         <input type="text" class="form-control" name="name" value="<?php if(isset($_POST['name']) && empty($nameErr)){ echo $_POST['name'];} else {echo '';}?>" required><span class="error"><?php echo $nameErr; ?><?php $_POST = array() ?></span>
                     </div>
                     <div class="form-group">
@@ -424,12 +291,12 @@
                     </div>
                     <div class="form-group">
                         <label for="questions">Questions</label>
-                        <textarea class="form-control" type="text" name="questions" rows="4" value="<?php $_POST = array() ?>"></textarea>
+                        <textarea id="filterta" class="form-control" type="text" name="questions" rows="4" value="<?php $_POST = array() ?>"></textarea>
                         
                         <small id="emailHelp" class="form-text text-muted">We'll never share your information with anyone else.</small>
                         
                     </div>
-                        <button type="submit" class="btn btn-primary" >Submit</button>
+                        <button type="submit" class="btn btn-primary" onclick="check_val()"; >Submit</button>
                 </form>
            </div>
     	</div>
@@ -463,6 +330,31 @@
     </footer>
     
 </body>
+
+<!-- Javascript Word Filter -->
+<script type="text/javascript">
+function check_val()
+{
+ var bad_words=new Array("death","kill","murder","cornflakes with oranges");
+ var check_text=document.getElementById("filterta").value;
+ var error=0;
+ for(var i=0;i<bad_words.length;i++)
+ {
+  var val=bad_words[i];
+  if((check_text.toLowerCase()).indexOf(val.toString())>-1)
+  {
+   error=error+1;
+  }
+ }
+	
+ if(error>0)
+ {
+  alert("Some Bad Words In Your Text!");
+  window.stop();
+ }
+}
+</script>
+
 
 </html>
 <?php ob_end_flush(); ?>
