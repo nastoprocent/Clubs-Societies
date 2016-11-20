830064dbbe1f52e8005b2746773010ad15bc2a46
x14303766 <bdoor.alshakaki@gmail.com>
2016-11-20 18:32:16 +0000

    music

--- music.php
+++ music.php
@@ -259,11 +259,13 @@
      	<div class="row">
             <div class="box">
                 <div class="col-lg-12 text-center">
+                    <audio autoplay>
+                        <source src="NCI.mp3" type="audio/mpeg">
+                    </audio>
                     <hr>
                             <h2 class="brand-before">
-                                <small>Welcome to</small>
+                            <small>Welcome to</small>
                             </h2>
-        
                             <h1 class="brand-name"> Music Society</h1>
                             <hr class="tagline-divider">
 <br />
